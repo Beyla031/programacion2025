aa004610a2dc9df40a9278233da0feada0faa767
Beyla031 <bmpirir@scl.edu.gt>
2025-04-21 19:50:03 -0600

    Add files via upload

---
+++ services/sql.py
@@ -0,0 +1,33 @@
+import mysql.connector
+def conectar(consulta_sql):
+ 
+
+#Configuración de la conexión/ credenciales
+    config = {
+        'user': 'uayaxakc47tovhih',
+        'password': '9yusDttY9F2uGNvnyxtE',
+        'host': 'bsr9zztwkuuvg74hhhhl-mysql.services.clever-cloud.com',
+        'database': 'bsr9zztwkuuvg74hhhhl',
+        'raise_on_warnings': True
+    }
+
+
+# Conectar a la base de datos 
+    try: 
+       conexion = mysql.connector.connect(**config)
+       print("Conexión exitosa a la base de datos.")
+
+      #Objeto para crear consultas
+       consultas = conexion.cursor()
+
+       #Función para agregar la consulta SQL
+       consultas.execute(consulta_sql)
+       #Almacenamos el resultado de la consulta SQL
+       resultado = consultas.fetchall()
+
+       return resultado
+     
+    #Respuesta si al conectar da error 
+    except mysql.connector.Error as err:
+       print(f"Error al conectar a la base de datos: {err}")
+
