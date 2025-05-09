196fd3e2a02908e5718e5e58f37de827ab0aba7a
Beyla031 <bmpirir@scl.edu.gt>
2025-05-08 18:38:29 -0600

    Add files via upload

--- views/formView.py
+++ views/formView.py
@@ -1,12 +1,12 @@
-#fromView.py
 import tkinter
 from views.datosView import actualizarTabla
+import tkinter.messagebox as messagebox
 
 def form_view(ventana):
     formulario_panel = tkinter.Frame(ventana, bg="yellow", width="300", height="600") 
     formulario_panel.grid(row=1, column=0, sticky="nsew")
 
-    tabla_panel = tkinter.Frame(ventana, bg="white", width="10",height="10")
+    tabla_panel = tkinter.Frame(ventana, bg="white", width="10", height="10")
     tabla_panel.grid(row=1, column=1, sticky="nsew")
 
     titulo = tkinter.Label(formulario_panel, text="Formulario de registro")
@@ -19,11 +19,46 @@ def form_view(ventana):
         respuesta = entry.get()
         actualizarTabla(f"SELECT * FROM datos_generales WHERE color ='{respuesta}'", tabla_panel)
 
-
-    actualizarTabla(f"SELECT * FROM datos_generales", tabla_panel)
-
     boton = tkinter.Button(formulario_panel, text="Enviar", command=funcion_boton)
     boton.pack(pady=5)
 
+    #HORA
+    titulo = tkinter.Label(formulario_panel, text="Filtrar por hora (rango)")
+    titulo.pack(pady=5)
+
+    entry_hora1 = tkinter.Entry(formulario_panel)
+    entry_hora1.pack(pady=5)
+
+    entry_hora2 = tkinter.Entry(formulario_panel)
+    entry_hora2.pack(pady=5)
+
+    def filtrar_por_hora():
+        h1 = entry_hora1.get().strip()
+        h2 = entry_hora2.get().strip()
+
+        if not h1.isdigit() or not h2.isdigit():
+            messagebox.showerror("Error ingresa hora valida")
+            return
+
+        h1 = int(h1)
+        h2 = int(h2)
+
+        if h1 > h2:
+            messagebox.showerror("Error", "La hora inicial no puede ser mayor que la final")
+            return
+
+        h1_formato = f"{h1:02d}:00"
+        h2_formato = f"{h2:02d}:00"
+
+        consulta = f"SELECT * FROM datos_generales WHERE hora_entrada BETWEEN '{h1_formato}' AND '{h2_formato}'"
+        actualizarTabla(consulta, tabla_panel)
+
+    boton_hora = tkinter.Button(formulario_panel, text="Filtrar hora", command=filtrar_por_hora)
+    boton_hora.pack(pady=5)
+
+
+    actualizarTabla(f"SELECT * FROM datos_generales", tabla_panel)
 
     return formulario_panel
+
+    
